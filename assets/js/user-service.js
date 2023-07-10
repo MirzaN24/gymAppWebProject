6b2776f59901d00dfbd0250751dfc261ad0b27c5
MirzaN24 <mirza.novalic@stu.ibu.edu.ba>
2023-07-10 14:34:38 +0200

    design updates

--- assets/js/user-service.js
+++ assets/js/user-service.js
@@ -1,107 +1,118 @@
 var UserService = {
 
     init: function () {
-      $('#addUserForm').validate({
-        submitHandler: function (form) {
-          var user = Object.fromEntries((new FormData(form)).entries()); //converting form to JSON
-          UserService.add(user);
-        }
-      });
-      UserService.list();
+        $('#addUserForm').validate({
+            submitHandler: function (form) {
+                var user = Object.fromEntries((new FormData(form)).entries()); //converting form to JSON
+                UserService.add(user);
+            }
+        });
+        UserService.list();
     },
 
     list: function () {
-      $.get("rest/user", function (data) {
+        $.get("rest/user", function (data) {
 
-        $("#user-cards").html("");
+            $("#user-cards").html("");
 
-        var html = "";
-        for (let i = 0; i < data.length; i++) { //only id and name, pw and email are not displayed
-          html += `
-            <div class="col-lg-4">
-            <!--I CAN ADD IMAGES OF USERS HERE-->
-            <h2 class="fw-normal">`+ data[i].id + `</h2>
-        <p>`+ data[i].first_name + `</p>
-            <p>`+ data[i].last_name + `</p>
-            <p> 
-          <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-primary see-user" onclick="UserService.get(`+ data[i].id + `)">EDIT</button>
-            <button type="button" class="btn btn-danger see-user" onclick="UserService.delete(`+ data[i].id + `)">DELETE</button>
-          </div>
-        </p>
-      </div> `;
-        }
-        $("#user-cards").html(html);
-      });
+            var html = "";
+            for (let i = 0; i < data.length; i++) { //only id and name, pw and email are not displayed
+                html += `
+            <div class="col-lg-3" style="margin-bottom: 20px; margin-top: 20px;">     
+                <div class="card" style="width: 18rem;">
+                    <img src="https://icons-for-free.com/iconfiles/png/512/customer+information+personal+profile+user+icon-1320086045331670685.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="text-align: center;">`+ data[i].id + `</h5>
+                        <p class="card-text">`+ data[i].first_name + `</p>
+                        <p class="card-text">`+ data[i].last_name + `</p>
+                        <div class="btn-group" role="group" style="display: flex; justify-content: center;">
+                            <button type="button" class="btn btn-primary see-user" onclick="UserService.get(`+ data[i].id + `)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                </svg>
+                            </button>
 
+                            <button type="button" class="btn btn-danger see-user" onclick="UserService.delete(`+ data[i].id + `)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                                </svg>
+                          </button>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+            }
+            $("#user-cards").html(html);
+        });
     },
 
     get: function (id) {
-      $('.see-user').attr('disabled', true);
-      $.get('rest/user/' + id, function (data) {
-        console.log(data);
-        $("#id").val(data.id);
-        $("#first_name").val(data.first_name);
-        $("#last_name").val(data.last_name);
-        $("#email").val(data.email);
-        $("#pass").val(data.pass);
-        $('#exampleModal').modal("show");
-        $('.see-user').attr('disabled', false);
-      })
+        $('.see-user').attr('disabled', true);
+        $.get('rest/user/' + id, function (data) {
+            console.log(data);
+            $("#id").val(data.id);
+            $("#first_name").val(data.first_name);
+            $("#last_name").val(data.last_name);
+            $("#email").val(data.email);
+            $("#pass").val(data.pass);
+            $('#exampleModal').modal("show");
+            $('.see-user').attr('disabled', false);
+        })
     },
 
     add: function (user) {
-      $.ajax({
-        url: 'rest/user',
-        type: 'POST',
-        data: JSON.stringify(user),
-        contentType: "application/json",
-        dataType: "json",
-        success: function (result) {
-          $('#user-cards').html('<div class="text-center"> <div class="spinner-border" role="status"> <span class="visually-hidden">Loading...</span> </div> </div>');
-          $('#addUserModal').modal("hide");
-          UserService.list(); //include performance optimization where only one user card refreshes instead of whole page
-        }
-      });
+        $.ajax({
+            url: 'rest/user',
+            type: 'POST',
+            data: JSON.stringify(user),
+            contentType: "application/json",
+            dataType: "json",
+            success: function (result) {
+                $('#user-cards').html('<div class="text-center"> <div class="spinner-border" role="status"> <span class="visually-hidden">Loading...</span> </div> </div>');
+                $('#addUserModal').modal("hide");
+                UserService.list(); //include performance optimization where only one user card refreshes instead of whole page
+            }
+        });
     },
 
     update: function () {
-      $('.save-user').attr('disabled', true);
+        $('.save-user').attr('disabled', true);
 
-      var user = {};
+        var user = {};
 
-      user.id = $('#id').val();
-      user.first_name = $('#first_name').val();
-      user.last_name = $('#last_name').val();
-      user.email = $('#email').val();
-      user.pass = $('#pass').val();
+        user.id = $('#id').val();
+        user.first_name = $('#first_name').val();
+        user.last_name = $('#last_name').val();
+        user.email = $('#email').val();
+        user.pass = $('#pass').val();
 
-      $.ajax({
-        url: 'rest/user/' + $('#id').val(),
-        type: 'PUT',
-        data: JSON.stringify(user),
-        contentType: "application/json",
-        dataType: "json",
-        success: function (result) {
-          $('#exampleModal').modal("hide");
-          $('.see-user').attr('disabled', false);
-          $('#user-cards').html('<div class="text-center"> <div class="spinner-border" role="status"> <span class="visually-hidden">Loading...</span> </div> </div>');
-          UserService.list(); //include performance optimization where only one user card refreshes instead of whole page
-        }
-      });
+        $.ajax({
+            url: 'rest/user/' + $('#id').val(),
+            type: 'PUT',
+            data: JSON.stringify(user),
+            contentType: "application/json",
+            dataType: "json",
+            success: function (result) {
+                $('#exampleModal').modal("hide");
+                $('.see-user').attr('disabled', false);
+                $('#user-cards').html('<div class="text-center"> <div class="spinner-border" role="status"> <span class="visually-hidden">Loading...</span> </div> </div>');
+                UserService.list(); //include performance optimization where only one user card refreshes instead of whole page
+            }
+        });
     },
 
     delete: function (id) {
-      $('.see-user').attr('disabled', true);
-      $.ajax({
-        url: 'rest/user/' + id,
-        type: 'DELETE',
-        success: function (result) {
-          $('#user-cards').html('<div class="text-center"> <div class="spinner-border" role="status"> <span class="visually-hidden">Loading...</span> </div> </div>');
-          UserService.list();
-        }
-      });
+        $('.see-user').attr('disabled', true);
+        $.ajax({
+            url: 'rest/user/' + id,
+            type: 'DELETE',
+            success: function (result) {
+                $('#user-cards').html('<div class="text-center"> <div class="spinner-border" role="status"> <span class="visually-hidden">Loading...</span> </div> </div>');
+                UserService.list();
+            }
+        });
     },
 
 
-  }
+}
