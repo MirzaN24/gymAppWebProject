5a565ab1452feffcf339ecb83a1019e01fe79f73
MirzaN24 <mirza.novalic@stu.ibu.edu.ba>
2023-07-12 01:24:47 +0200

    jwt stuff

--- rest/routes/UserRoutes.php
+++ rest/routes/UserRoutes.php
@@ -1,7 +1,28 @@
 <?php
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 Flight::route('/', function(){
     echo "Hello world!";
+});
+
+Flight::route('GET /jwt', function(){
+
+$key = 'example_key';
+$payload = [
+    'iss' => 'http://example.org',
+    'aud' => 'http://example.com',
+    'iat' => 1356999524,
+    'nbf' => 1357000000
+];
+
+$jwt = JWT::encode($payload, $key, 'HS256');
+$decoded = (array) JWT::decode($jwt, new Key($key, 'HS256'));
+
+print_r($jwt);
+print_r($decoded);
+
 });
 
 Flight::route('GET /user', function(){
