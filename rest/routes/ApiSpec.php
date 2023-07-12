b6ad4cc2465c27b221a6e886a21ed21c2d13ff36
MirzaN24 <mirza.novalic@stu.ibu.edu.ba>
2023-07-13 00:30:41 +0200

    swagger update

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -3,8 +3,8 @@
 /**
  * @OA\Info(title="BestGym API Specs", version="0.1", @OA\Contact(email="mirza.novalic@stu.ibu.edu.ba", name="Mirza Novalic"))
  * @OA\OpenApi(
- *    @OA\Server(url="http://localhost/gymAppWebProject/login.html", description="Development Environment" ),
- *    @OA\Server(url="https://lobster-app-y8bzx.ondigitalocean.app/login.html", description="Production Environment" )
+ *    @OA\Server(url="http://localhost/gymAppWebProject/rest", description="Development Environment" ),
+ *    @OA\Server(url="https://lobster-app-y8bzx.ondigitalocean.app/rest", description="Production Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
  */
